2b0222c28437cd4ab59392f74c2a0bcd0a1caff7
Andreea Gardelean <andreea.gardelean.2020@live.rhul.ac.uk>
2024-10-09 20:53:46 +0100

    Created a function to handle file upload. The function will retrieve the file name and change the label of the input field to its name

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import '../style/input.css';
 
 /**
@@ -6,14 +6,25 @@ import '../style/input.css';
  * @returns {JSX.Element} A styled file input component.
  */
 export default function Input() {
+  const [label, setLabel] = useState('Upload a PDF File...');
+  const [file, setFile] = useState();
+
   function filePicker() {
     document.getElementById("pdfFile")?.click();
   }
 
+  function handleFileUpload(e) {
+    const file = e.target.files?.[0];
+    if (file) {
+      setLabel(file.name);
+      setFile(file);
+    }
+  }
+
   return (
     <div className="picker-container">
-      <div className="picker" onClick={filePicker}>Upload a PDF File...</div>
-      <input type="file" id="pdfFile" name="pdfFile" accept=".pdf" />
+      <div className="picker" onClick={filePicker}>{ label }</div>
+      <input type="file" id="pdfFile" name="pdfFile" accept=".pdf" onChange={handleFileUpload} />
     </div>
   );
 }
