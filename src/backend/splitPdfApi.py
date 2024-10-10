788f2ce3feff80f487112f9260366d323f628f75
Andreea Gardelean <andreea.gardelean.2020@live.rhul.ac.uk>
2024-10-10 12:15:53 +0100

    Made a POST request the the API to split the uploaded PDF document. The response is received as a blob, which is then downloaded asa a zip file automatically.

---
+++ src/backend/splitPdfApi.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request, send_file
+from flask_cors import CORS
+from splitPdf import splitPdf
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-type'
+
+"""
+Flask application for splitting PDF files into individual pages and returning them as a ZIP file.
+
+POST /split: 
+  Expect a file to be passed from the client as form data.
+        
+  Returns:
+    - zip file containing the split Pdf files. 
+    - JSON response containing an error message if no file has been sent from the client.
+"""
+@app.route('/split', methods=["POST"])
+def split():
+  if request.method == "POST":
+    if 'pdfFile' not in request.files:
+      return jsonify({'error': 'No file attached'})
+    
+    file = request.files['pdfFile']
+    if file.filename == '':
+      return jsonify({'error': 'No file attached'})
+    
+    result = splitPdf(file)
+    
+    return send_file(result, as_attachment=True, download_name=f'payslips.zip', mimetype='application/zip')
+
+if __name__ == "__main__":
+  app.run()
--- src/components/Input.js
+++ src/components/Input.js
@@ -8,21 +8,84 @@ import paperclip from '../icons/paperclip.svg';
  */
 export default function Input() {
   const [label, setLabel] = useState('Upload a PDF File...');
-  const [file, setFile] = useState();
   const [btnContent, setBtnContent] = useState('Upload File');
   const [visibleFilename, setVisibleFilename] = useState('');
 
+   /**
+   * Triggers the click event for the hidden file input element, allowing users to select a file.
+   */
   function filePicker() {
     document.getElementById("pdfFile")?.click();
   }
 
+  /**
+   * Handles the file upload change event. 
+   * Updates the component's state with the new file information and calls the API to process the uploaded file.
+   *
+   * @param {Event} e - The event object from the file input change event.
+   */
   function handleFileUpload(e) {
-    const file = e.target.files?.[0];
-    if (file) {
-      setLabel(file.name);
-      setFile(file);
+    const newFile = e.target.files?.[0];
+    if (newFile) {
+      setLabel(newFile.name);
       setBtnContent('Replace');
       setVisibleFilename('visible');
+      splitPdf(newFile);
+    }
+  }
+
+  /**
+   * Formats the current date as 'DD-MM-YYYY'.
+   *
+   * @returns {string} The formatted date string.
+   */
+  function getFormattedDate() {
+    const date = new Date();
+    const dateFormat = {
+      day: String(date.getDate()).padStart(2, '0'),
+      month: String(date.getMonth() + 1).padStart(2, '0'),
+      year: date.getFullYear()
+    }
+
+    const zipName = `${dateFormat.day}-${dateFormat.month}-${dateFormat.year}`;
+    
+    return zipName;
+  }
+
+  /**
+   * Sends the uploaded file to the API for processing. 
+   * Receives a directory of split PDF files as a blob and triggers the download for the user.
+   *
+   * @param {File} file - The PDF file to be sent to the API.
+   */
+  async function splitPdf(file) {
+    // create a FormData object to hold the file
+    const formData = new FormData();
+    formData.append('pdfFile', file);
+
+    try {
+      const request = await fetch('http://127.0.0.1:5000/split', {
+        method: 'POST',
+        body: formData
+      })
+
+      // get the response as a blob
+      const response = await request.blob();
+      const url = window.URL.createObjectURL(response);
+      const link = document.createElement('a');
+      document.body.appendChild(link);
+
+      // download the received blob as a zip file
+      const zipName = getFormattedDate();
+
+      link.style = "display: none";
+      link.href = url;
+      link.download = `Payslips-${zipName}.zip`;
+      link.click();
+      window.URL.revokeObjectURL(url);
+
+    } catch (e) {
+      console.error('Error during file upload split', e);
     }
   }
 
