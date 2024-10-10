0021dfe7add07430d81626a159b1fa7f404b515a
Andreea Gardelean <andreea.gardelean.2020@live.rhul.ac.uk>
2024-10-10 12:15:10 +0100

    Modified the splitPdf functionality to accept a file instead of a filepath. Then a zip file is created in-memory which is then used to save the split PDFs. Once all the pages of the PDF are split the zip file is returned

--- src/backend/splitPdf.py
+++ src/backend/splitPdf.py
@@ -1,6 +1,7 @@
 from PyPDF2 import PdfReader, PdfWriter
-import os
 from datetime import date
+import zipfile 
+from io import BytesIO
 
 """
 Gets the current date and formats it from YYYY-MM-DD to DD-MM-YYYY.
@@ -13,66 +14,40 @@ def getDate():
     return today.strftime('%d-%m-%Y')
 
 """
-Creates a directory with the given name.
-
-Args:
-    directoryName (str): The name of the directory to create.
-
-Raises:
-    PermissionError: If the user doesn't have permission to create the directory.
-    Exception: Catches any other exceptions that may occur during directory creation.
-"""
-def createDirectory(directoryName):
-    try:
-        os.mkdir(directoryName)
-    except PermissionError:
-        print('Unable to create directory. Reason: PERMISSION DENIED')
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-"""
-Extracts and returns the name of the employee from a given PDF page.
-
-Args:
-    page (PyPDF2.PageObject): The page of the PDF from which to extract the employee's name.
-
-Returns:
-    str: The extracted name of the employee (or any available text from the page).
-"""
-def findEmployeeName(page):
-    name = page.extract_text()
-    return name
-
-"""
 Splits a PDF file into individual pages, saves each page as a separate PDF file,
-and stores them in a newly created directory. The directory is named with the prefix
-'Payslips' followed by the current date in DD-MM-YYYY format.
+and stores them in a newly created ZIP file in memory using zipfile and BytesIO. 
+The ZIP file is named with the prefix 'Payslips' followed by the current date in DD-MM-YYYY format.
 
 Each new PDF file is named using the page number and the current date.
 
-Raises:
-    FileNotFoundError: If the input PDF file doesn't exist.
-    Exception: Catches any other exceptions that may occur during the PDF processing.
+Args:
+    file (BytesIO): The input PDF file to be split.
+
+Returns:
+    BytesIO: A BytesIO object containing the ZIP file with individual PDF pages.
 """
-def splitPdf(filepath):
-    with open(filepath, "rb") as inputPdfFile:
-        inputPdf = PdfReader(inputPdfFile)
-        totalPages = len(inputPdf.pages)
-        todayDate = getDate()
-        directoryName = 'Payslips ' + todayDate
+def splitPdf(file):
+    inputPdf = PdfReader(file)
+    totalPages = len(inputPdf.pages)
 
-        createDirectory(directoryName)
+    zipBuffer = BytesIO()
+    todayDate = getDate()
 
+    with zipfile.ZipFile(file=zipBuffer, mode="w", compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zipArchive:
         # iterate over each page of the pdf and create a pdf for each
         for page in range(totalPages):
             currPage = inputPdf.pages[page]
-            
             filename = f"{page}-{todayDate}.pdf"
-            findEmployeeName(currPage)
 
             outputPdf = PdfWriter()
             outputPdf.add_page(currPage)
 
-            outputPdfPath = os.path.join(directoryName, filename)
-            with open(outputPdfPath, "wb") as outputPdfFile:
-                outputPdf.write(outputPdfFile)
+            pdfBytes = BytesIO()
+            outputPdf.write(pdfBytes)
+            pdfBytes.seek(0)
+
+            # write the PDF to the in-memory zip file
+            zipArchive.writestr(filename, pdfBytes.read())
+
+    zipBuffer.seek(0)
+    return zipBuffer
