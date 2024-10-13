2c272d471d0dfcd7b4c885f98783b819043f8a16
andreeagardelean <gardelean.andreea@yahoo.com>
2024-10-13 15:05:49 +0100

    Updated application header information

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,26 @@ import '../style/header.css';
 
 /**
  * Header component for the PDF Splitter application.
- * This component displays a welcome message and instructions 
- * for using the PDF splitter app. It includes a container 
- * with a header element that has a greeting message and instructions 
+ * This component displays a welcome message and instructions
+ * for using the PDF splitter app. It includes a container
+ * with a header element that has a greeting message and instructions
  * for uploading a PDF.
  *
  * @returns {JSX.Element} The rendered Header component.
  */
 export default function Header() {
-  return (
-    <div className="header-container">
-      <header className="header">
-        <p className='greeting'>
-          Welcome to PDF Splitter!
-        </p>
-        <p>
-          To make use of this application, upload below a single PDF file and the app will break each page of the PDF file into a PDF file on its own.
-        </p>
-      </header>
-    </div>
-  )
+	return (
+		<div className="header-container">
+			<header className="header">
+				<p className="greeting">Welcome to PDF Editor!</p>
+				<p>
+					To use the application, upload a PDF file and preview its content. Then, select the pages
+					you want to either download or exclude from the original document. You can choose to
+					download only the selected pages or generate a new PDF that excludes them. For more
+					information see the
+					<a href="https://github.com/AndreeaGardelean/pdf-editor">application notes</a>.
+				</p>
+			</header>
+		</div>
+	);
 }
